b10aa47d25b30bacfc282e4dbf6f284a3509a226
basuka <reon5525@gmail.com>
2023-09-01 01:22:57 +0900

    Updates

--- assets/About-e11a04b5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t}from"./index-89e3b93d.js";import"https://esm.sh/@octokit/rest";const c={};function r(n,s){return o(),t("div",null,"Aboutページです")}const f=e(c,[["render",r]]);export{f as default};
